45ce7262543ba9f6abc3e0029d3e3bf965d3f424
malykh.gn <germanmalyh@gmail.com>
2022-08-07 16:18:23 +0300

    add DragAndDropTest

---
+++ src/test/java/internet/DragAndDropTest.java
@@ -0,0 +1,27 @@
+package internet;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.*;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropTest {
+
+    @BeforeAll
+    static void configure() {
+        Configuration.baseUrl = "https://the-internet.herokuapp.com";
+    }
+
+    @Test
+    void rectangleSwap() {
+        //Open Url
+        open("/drag_and_drop");
+        //Swap A and B
+        $("#column-a").dragAndDropTo($("#column-b"));
+        //Check column-a name
+        $("#column-a").shouldHave(text("B"));
+        //Check column-b name
+        $("#column-b").shouldHave(text("A"));
+    }
+}
